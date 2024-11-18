bc26219fc5d72edc31cc1f384f77dcccb8fa9ab6
Badro63 <ibadreldin24@gmail.com>
2024-11-18 18:02:32 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+let toggleBtn = document.querySelector(".menu-toggle");
+let menu = document.querySelector(".menu");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
